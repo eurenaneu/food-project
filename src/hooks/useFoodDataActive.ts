e58a4b45a467bb8355c595043664c9be9481bdbb
Renan Machado <renanpando@gmail.com>
2023-11-13 15:44:20 -0300

    adicionado: método get para produtos ativos

---
+++ src/hooks/useFoodDataActive.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosPromise } from "axios"
+import { FoodData } from "../interface/FoodData"
+import { useQuery } from "@tanstack/react-query"
+
+const API_URL = "http://localhost:8080"
+
+const fetchData = async (): AxiosPromise<FoodData[]> => {
+    const response = axios.get(API_URL + "/food/active")
+
+    return response;
+}
+
+export function useFoodDataActive() {
+    const query = useQuery({
+        queryFn: fetchData,
+        queryKey: ['food-data'],
+        retry: 2,
+        onError: (error) => {
+            if (axios.isAxiosError(error)) {
+                if (error.code === "ERR_NETWORK") {
+                    console.error("Não foi possivel realizar a conexão com a API!")
+                }
+            }
+        }
+    })
+    
+    return { 
+        ...query,
+        data: query.data?.data
+    }
+}
