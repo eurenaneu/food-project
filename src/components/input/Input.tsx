b4924817fe586b4ae1bd40aaadaab4d3aeaf7429
Renan Machado <renanpando@gmail.com>
2023-11-13 15:39:24 -0300

    corrigido: id para evitar conflitos com modal

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -3,26 +3,27 @@ import { forwardRef } from 'react'
 import { InputHTMLAttributes } from 'react'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
-    className?: string
+    className?: string,
+    id: string,
     type: string,
     label: string,
     placeholder?: string
 }
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(({ className, type, label, placeholder, ...props }, ref) => {
+export const Input = forwardRef<HTMLInputElement, InputProps>(({ className, id, type, label, placeholder, ...inputProps }, ref) => {
     return(
         <>
-            <label htmlFor="form-input">{label}
+            <label htmlFor={id}>{label}
                 <input
                     className={className}
                     type={type}
                     placeholder={placeholder}
                     min="0"
                     step="0.01"
-                    id="form-input"
+                    id={id}
                     name="form-input"
                     ref={ref}
-                    {...props}
+                    {...inputProps}
                 />
             </label>
         </>
