0f45ba1686a10daaac2cbc619f6e6bb283c7add8
Renan Machado <renanpando@gmail.com>
2023-11-13 15:22:51 -0300

    atualizado: evento de clique para futura feature

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,15 +1,22 @@
+import { HTMLAttributes } from 'react'
 import styles from './Card.module.css'
 
-interface CardProps {
+interface CardProps extends HTMLAttributes<HTMLDivElement> {
     nome: string,
     valor: string
 }
 
 export function Card({ nome, valor } : CardProps){
+    const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+        event.currentTarget.classList.toggle(styles.active)
+    }
+
     return(
-        <div className={styles.card}>
+        <div className={styles.card} onClick={handleClick}>
             <h2>{nome}</h2>
-            <p>R${valor}</p>
+            <div className={styles.valores}>
+                <p>R${valor}</p>
+            </div>
         </div>
     )
 }
