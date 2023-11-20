7bd9ba1fd096baa64fb3e9460c9f2eb6466e8cb9
Renan Machado <renanpando@gmail.com>
2023-11-20 15:00:11 -0300

    atualizado: componentização do modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,79 +1,24 @@
-import styles from "./Modal.module.css"
-import { Button } from "../button/Button";
-import { Input } from "../input/Input";
+import styles from "./Modal.module.css";
 import { FoodData } from "../../interface/FoodData";
-import { useForm } from "react-hook-form";
-import { useEffect } from "react";
-import { useFoodDataPatch } from "../../hooks/useFoodDataPatch";
+import { DeleteModal } from "./delete-modal/DeleteModal";
+import { EditModal } from "./edit-modal/EditModal";
 
 interface ModalProps {
     isOpen: boolean,
     setOpen: (isOpen: boolean) => void,
-    modalData: FoodData
+    modalData: FoodData,
+    type: string,
 }
 
-interface FormData {
-    nome: string,
-    valor: number
-}
-
-export function Modal({ isOpen, setOpen, modalData } : ModalProps) {
-    useEffect(() => {
-        reset(modalData);
-    }, [modalData]);
-    
-    const { register, handleSubmit, reset, formState: { errors } } = useForm<FormData>()
-
-    const { mutate } = useFoodDataPatch();
-
-    const handleForm = (data: FormData) => {
-        mutate(data);
-        setOpen(!open)
-    }
-    
+export function Modal({ isOpen, setOpen, modalData, type } : ModalProps) {
     if (isOpen) {
         return (
             <div className={styles.overlay}>
-                <div className={styles.modal}>
-                    <h1 className={styles.title}>Alterar prato</h1>
-                    
-                    <form onSubmit={handleSubmit(handleForm)}>
-                        <div className={styles["input-fields"]}>
-                            <div className={styles["form-group"]}>
-                                <Input
-                                    className={errors?.nome && styles["input-error"]}
-                                    {...register("nome", { required: true })}
-                                    type="text"
-                                    label="Nome do prato"
-                                    id="novo-nome"
-                                    defaultValue={modalData.nome}
-                                />
+                {type == "edit" && 
+                <EditModal modalData={modalData} setOpen={setOpen} isOpen={isOpen}/>}
 
-                                {errors?.nome?.type === "required" && <p className={styles['error-message']}>Nome não deve estar vazio!</p>}
-                            </div>
-
-                            <div className={styles["form-group"]}>
-                                <Input
-                                    className={errors?.valor && styles["input-error"]}
-                                    {...register("valor", { required: true, min: 0.01 })}
-                                    type="number"
-                                    label="Preço"
-                                    id="novo-preco"
-                                    placeholder="0,00"
-                                    defaultValue={modalData.valor}
-                                />
-
-                                {errors?.valor?.type === "min" && <p className={styles['error-message']}>Preço deve ser maior que 0!</p>}
-                                {errors?.valor?.type === "required" && <p className={styles['error-message']}>Preço não deve estar vazio!</p>}
-                            </div>
-                        </div>
-                        
-                        <div className={styles["button-fields"]}>
-                            <Button type="submit" className={styles["edit-button"]} text="alterar" />
-                            <Button className={styles["cancel-button"]} onClick={() => setOpen(!open)} text="cancelar" />
-                        </div>
-                    </form>
-                </div>
+                { type == "delete" &&
+                <DeleteModal modalData={modalData} setOpen={setOpen} isOpen={isOpen}/>}
             </div>
         )
     } else {
